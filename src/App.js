2c223e18ad7cadebaaa29153b485d5d5f41e870b
Max Huckstepp <mhuckstepp@gmail.com>
2022-01-26 22:20:05 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Auth with Strava to get new title for your activities!</p>
+    </div>
+  );
+}
+
+export default App;
