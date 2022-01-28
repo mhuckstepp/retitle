9463da18b3255b78382cdf57a400aba508f0bcc2
Max Huckstepp <mhuckstepp@gmail.com>
2022-01-28 15:47:00 -0800

    copy change

--- src/components/Login.component.js
+++ src/components/Login.component.js
@@ -7,7 +7,9 @@ const Login = () => {
   return (
     <CenteredDiv>
       <h2>Tired of boring/generic activity titles on Strava?</h2>
-      <h2>Use ReTitle to get a new fun new title for your activities!</h2>
+      <h2>
+        Use ReTitle to get a fun new title for your activities automatically!
+      </h2>
       <Button
         variant="contained"
         href={CONSTANTS.STRAVA_AUTH_LINK}
