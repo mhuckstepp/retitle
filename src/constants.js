67504b51def11df992b7785bb5403f2de0858f53
Max Huckstepp <mhuckstepp@gmail.com>
2022-01-28 14:28:42 -0800

    add env variables

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const CONSTANTS = {
-  STRAVA_AUTH_LINK: `http://www.strava.com/oauth/authorize?client_id=77230&response_type=code&redirect_uri=http://localhost:3000/settings&approval_prompt=auto&scope=activity:read_all,activity:write,profile:read_all`,
+  STRAVA_AUTH_LINK: `http://www.strava.com/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_STRAVA_REDIRECT}&approval_prompt=auto&scope=activity:read_all,activity:write,profile:read_all`,
 };
