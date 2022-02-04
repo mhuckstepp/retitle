86ae5f082676de2e5342d97d4f5536fc7cc8003f
Andrew Southard <andrewsouthard@users.noreply.github.com>
2022-02-03 20:16:46 -0500

    Update API URL

--- src/bootstrap/axios.js
+++ src/bootstrap/axios.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "https://gh4p3b37na.execute-api.us-east-1.amazonaws.com/users";
+const BASE_URL = 'https://0zprthcval.execute-api.us-east-1.amazonaws.com/users';
 
 export const axiosCodeClient = axios.create({
   baseURL: BASE_URL,
-  method: "post",
+  method: 'post',
 });
