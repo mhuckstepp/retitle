c9e7a7c80289c2ed7c60ed1b6500a1fcb91d8a21
mhuckstepp <mhuckstepp@gmail.com>
2022-02-07 13:46:50 -0800

    env url cors

--- src/bootstrap/axios.js
+++ src/bootstrap/axios.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://0cj8ibd6nb.execute-api.us-east-1.amazonaws.com/users';
-
 export const axiosCodeClient = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.REACT_APP_API_UR,
   method: 'post',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
-    mode: 'no-cors',
   }
 });
