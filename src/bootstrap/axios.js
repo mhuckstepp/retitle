0476869cc62640a0b603e8a17ed1b299fc03b31d
mhuckstepp <mhuckstepp@gmail.com>
2022-02-06 20:10:25 -0800

    cors

--- src/bootstrap/axios.js
+++ src/bootstrap/axios.js
@@ -5,4 +5,9 @@ const BASE_URL = 'https://lnom9rm6x0.execute-api.us-east-1.amazonaws.com/users';
 export const axiosCodeClient = axios.create({
   baseURL: BASE_URL,
   method: 'post',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+    mode: 'no-cors',
+  }
 });
